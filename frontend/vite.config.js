7945a59ac8a642d4e68a1497b782eb2deb8d0030
Piyush Singh <singhpiyush48136@gmail.com>
2026-09-07 21:10:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://skillbridge-ai-1uy8.onrender.com',
         changeOrigin: true
       }
     }
